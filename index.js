e7a9190647e795c5a4b1340ce8ff65250f684639
johnthairukamau <tkamau82@gmail.com>
2023-03-21 22:41:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const companyName ='Scuber';
+
+let neighborhood = mostProfitableNeighborhood();
+
+function mostProfitableNeighborhood() {
+  return "Chelsea";
+}
+
+console.log(mostProfitableNeighborhood()); 
+
+let mostProfitableNeighborhood ='Chelsea';
+
+function setCompanyCeo() {
+  let companyCeo = "Susan Smith";
+  console.log("The CEO of the company is " + companyCeo);
+}
+
+setCompanyCeo();
